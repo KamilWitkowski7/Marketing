ec1d9552cfea1938ea04910b253684167b647f7b
Kamil Witkowski <kamil.witkowski@euro.com.pl>
2021-07-09 23:21:53 +0200

    chore: init

---
+++ src/test/kotlin/com/kupowanie/marketing/MarketingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kupowanie.marketing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
